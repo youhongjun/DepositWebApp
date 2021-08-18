805649ea651c5ab5f5af96e0780b0b2fbd246061
Hongjun <youhongjun@gmail.com>
2021-08-19 08:32:40 +1000

    Added blank deposit page.

--- DepositWebApp/Startup.cs
+++ DepositWebApp/Startup.cs
@@ -50,7 +50,7 @@ namespace DepositWebApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Deposit}/{action=Index}/{id?}");
             });
         }
     }
